66efd99e7a2e56395be416c94b47ddacc1d82c13
jinxing <jinxingwang@sohu-inc.com>
2020-01-02 16:14:06 +0800

    test.java

---
+++ src/main/java/leetcode/test.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+/**
+ * Created by oukinsei on 2020/1/2.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("first test");
+    }
+}
