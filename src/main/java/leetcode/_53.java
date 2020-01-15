9bcf6b6aeb836da9b92ddd92250102358f623df3
jinxing <jinxingwang@sohu-inc.com>
2020-01-15 16:06:58 +0800

    优化

--- src/main/java/leetcode/_53.java
+++ src/main/java/leetcode/_53.java
@@ -20,4 +20,14 @@ public class _53 {
         }
         return result;
     }
+
+    public int maxSubArray1(int[] nums) {
+        int result = Integer.MIN_VALUE;//优化上述代码时，忘记修改result的初始值导致出错
+        int maxLast=0;
+        for(int num : nums){
+            maxLast = Math.max(maxLast + num, num);
+            result = Math.max(maxLast, result);
+        }
+        return result;
+    }
 }
